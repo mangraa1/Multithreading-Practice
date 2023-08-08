16f00476071b2d07a71bfc40ac642c2e749d7329
mangraa1 <georgiu.spare@gmail.com>
2023-08-08 11:09:27 +0300

    Thread & Pthread practice

---
+++ Thread & Pthread/Thread & Pthread.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+//Thread
+//Operation
+//GCD
+
+//------Parallel------
+// Thread1 ------
+// Thread2 ------
+
+//------Сonsecutive------
+// Thread1 - - - - - -
+// Thread2  - - - - -
+
+//------Async------
+// 1Main(UI) -----
+// 2Thread     -
+
+
+//Unix - POSIX
+
+var thread = pthread_t(bitPattern: 0) // Create thread
+var attribut = pthread_attr_t()
+
+pthread_attr_init(&attribut)
+pthread_create(&thread, &attribut, { pointer in
+    print("test1")
+    return nil
+}, nil)
+
+// Thread
+
+var nsthread = Thread  {
+    print("test2")
+}
+nsthread.start()
