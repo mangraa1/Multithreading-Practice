2e9f12ac9525ceb984aaeddbd3de12f2d86092dc
mangraa1 <georgiu.spare@gmail.com>
2023-12-15 17:22:59 +0200

    [GCD Course] async after

--- Grand Central Dispatch Course/GCD/GCD/SecondViewController.swift
+++ Grand Central Dispatch Course/GCD/GCD/SecondViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    //MARK: - @IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
+    //MARK: - Internal vars
     fileprivate var imageURL: URL?
     fileprivate var image: UIImage? {
         get {
@@ -26,12 +28,18 @@ class SecondViewController: UIViewController {
         }
     }
 
+    //MARK: - Life Cyce
     override func viewDidLoad() {
         super.viewDidLoad()
 
         fetchImage()
+
+        delay(3) {
+            self.loginAlert()
+        }
     }
 
+    //MARK: - Internal logic
     fileprivate func fetchImage() {
         imageURL = URL(string: "https://wallpapers.com/images/featured-full/beautiful-3vau5vtfa3qn7k8v.jpg")
         activityIndicator.startAnimating()
@@ -43,6 +51,36 @@ class SecondViewController: UIViewController {
             DispatchQueue.main.async {
                 self.image = UIImage(data: data)
             }
+        }
+    }
+
+    fileprivate func loginAlert() {
+        let ac = UIAlertController(title: "Registered?", message: "Enter login and password", preferredStyle: .alert)
+
+        ac.addTextField { loginTextField in
+            loginTextField.placeholder = "Login"
+        }
+
+        ac.addTextField { passwordTextField in
+            passwordTextField.placeholder = "Passworg"
+            passwordTextField.isSecureTextEntry = true
+        }
+
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in
+            guard let login = ac.textFields?.first?.text,
+                  let password = ac.textFields?.last?.text
+            else { return }
+
+            print("login: \(login), password: \(password)")
+        }
+
+        ac.addAction(okAction)
+        present(ac, animated: true)
+    }
+
+    fileprivate func delay(_ delay: Int, completion: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            completion()
         }
     }
 }
